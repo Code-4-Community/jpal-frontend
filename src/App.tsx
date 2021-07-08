f3dcf79d5969c7aa4f773af828c348653cad3bb3
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-07-08 11:10:54 -0400

    changes to prettier config + cool libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import * as React from "react";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Logo } from "./Logo";
+import * as React from 'react';
+import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme } from '@chakra-ui/react';
+import ColorModeSwitcher from './components/ColorModeSwitcher';
+import { Logo } from './Logo';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
