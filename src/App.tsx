e38fc1c76f6ac37a02dea89128b9f1212fa0468f
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-08-12 15:56:56 -0400

    use id token not access token

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
+import Amplify, { Auth } from 'aws-amplify';
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { withAuthenticator, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
-import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import Landing from './Landing';
 
@@ -11,21 +11,24 @@ const queryClient = new QueryClient();
 Amplify.configure(awsconfig);
 
 const App: React.FC = () => {
-
-  Auth.currentSession().then(res=>{
-    const accessToken = res.getAccessToken()
-    const jwt = accessToken.getJwtToken()
+  console.log('making request...')
+  Auth.currentSession().then((res) => {
+    console.log(res);
+    const accessToken = res.getIdToken();
+    const jwt = accessToken.getJwtToken();
     // You can print them to see the full objects
-    console.log(`myAccessToken: ${JSON.stringify(accessToken)}`)
-    console.log(`myJwt: ${jwt}`)
-  })
+    console.log(`myIdToken: ${JSON.stringify(accessToken)}`);
+    console.log(`myJwt: ${jwt}`);
+  });
 
-   return <QueryClientProvider client={queryClient}>
-    <ChakraProvider theme={theme}>
-      <Landing />
-      <AmplifySignOut />
-    </ChakraProvider>
-  </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Landing />
+        <AmplifySignOut />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default withAuthenticator(App);
