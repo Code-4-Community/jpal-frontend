207836127d67e551e65465ce879dedd6ad113ef9
onb6 <blierolivia@gmail.com>
2021-08-11 20:44:49 -0400

    amplify stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { withAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 import Landing from './Landing';
 
 const queryClient = new QueryClient();
 
+Amplify.configure(awsconfig);
+
 const App: React.FC = () => (
   <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
+      <AmplifySignIn />
       <Landing />
     </ChakraProvider>
   </QueryClientProvider>
 );
 
-export default App;
+export default withAuthenticator(App);
