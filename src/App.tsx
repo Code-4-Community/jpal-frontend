1f912f75cb93ce3dc5a452ad742ddc569cc9a1b3
Vedant Rautela <v.rautela01@gmail.com>
2021-08-11 21:13:28 -0400

    got aws and jwt tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { withAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
-import Amplify from 'aws-amplify';
+import { withAuthenticator, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
+import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import Landing from './Landing';
 
@@ -10,13 +10,22 @@ const queryClient = new QueryClient();
 
 Amplify.configure(awsconfig);
 
-const App: React.FC = () => (
-  <QueryClientProvider client={queryClient}>
+const App: React.FC = () => {
+
+  Auth.currentSession().then(res=>{
+    const accessToken = res.getAccessToken()
+    const jwt = accessToken.getJwtToken()
+    // You can print them to see the full objects
+    console.log(`myAccessToken: ${JSON.stringify(accessToken)}`)
+    console.log(`myJwt: ${jwt}`)
+  })
+
+   return <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
-      <AmplifySignIn />
       <Landing />
+      <AmplifySignOut />
     </ChakraProvider>
-  </QueryClientProvider>
-);
+  </QueryClientProvider>;
+};
 
 export default withAuthenticator(App);
