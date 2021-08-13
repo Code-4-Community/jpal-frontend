1b4a570107b8e933773677ffd0035c80ade86646
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-08-13 10:15:09 -0400

    use reccommended amplify auth setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { AmplifyAuthenticator, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import Amplify from 'aws-amplify';
 import * as React from 'react';
@@ -10,17 +11,40 @@ const queryClient = new QueryClient();
 
 Amplify.configure(awsconfig);
 
-const App: React.FC = () => (
+const App: React.FC = () => {
   // Auth.currentSession().then((res) => {
   //   const accessToken = res.getIdToken();
   //   const jwt = accessToken.getJwtToken();
   // });
 
-  <QueryClientProvider client={queryClient}>
-    <ChakraProvider theme={theme}>
-      <Landing />
-      <AmplifySignOut />
-    </ChakraProvider>
-  </QueryClientProvider>
-);
-export default withAuthenticator(App);
+  const [authState, setAuthState] = React.useState<AuthState>();
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const [user, setUser] = React.useState<object | undefined>();
+
+  React.useEffect(
+    () =>
+      onAuthUIStateChange((nextAuthState, authData) => {
+        setAuthState(nextAuthState);
+        setUser(authData);
+      }),
+    [],
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        {authState === AuthState.SignedIn && user ? (
+          <div className="App">
+            <Landing />
+            <AmplifySignOut />
+          </div>
+        ) : (
+          <AmplifyAuthenticator usernameAlias="email">
+            <AmplifySignIn usernameAlias="email" hideSignUp slot="sign-in" />
+          </AmplifyAuthenticator>
+        )}
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+};
+export default App;
