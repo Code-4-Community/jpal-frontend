aa739d5217410cce7f10d83c17aaf3a654c68c29
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-08-18 18:19:51 -0400

    add sentry user logging and master failure notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import { AmplifyAuthenticator, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
+import * as Sentry from '@sentry/react';
 import Amplify from 'aws-amplify';
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -13,20 +14,21 @@ const queryClient = new QueryClient();
 Amplify.configure(awsconfig);
 
 const App: React.FC = () => {
-  // Auth.currentSession().then((res) => {
-  //   const accessToken = res.getIdToken();
-  //   const jwt = accessToken.getJwtToken();
-  // });
-
   const [authState, setAuthState] = React.useState<AuthState>();
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  const [user, setUser] = React.useState<object | undefined>();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [user, setUser] = React.useState<any>();
 
   React.useEffect(
     () =>
-      onAuthUIStateChange((nextAuthState, authData) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      onAuthUIStateChange((nextAuthState, authData: any) => {
         setAuthState(nextAuthState);
         setUser(authData);
+        if (authData?.attributes?.email) {
+          Sentry.setUser({ email: authData.attributes.email });
+        } else {
+          Sentry.setUser(null);
+        }
       }),
     [],
   );
