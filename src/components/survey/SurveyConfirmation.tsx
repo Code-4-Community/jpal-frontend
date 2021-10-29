3b27f1a10427beacbe05867b29ea375c8fc4078e
DerekL27 <dleung2140@gmail.com>
2021-10-29 14:26:39 -0400

    Fixed eslint errors

--- src/components/survey/SurveyConfirmation.tsx
+++ src/components/survey/SurveyConfirmation.tsx
@@ -5,12 +5,10 @@ import { Box, Button, VStack } from '@chakra-ui/react';
 const SurveyConfirmation: React.FC = () => (
   // eslint-disable-next-line no-alert
   <VStack justify="center" height="full">
-    <Box padding="4" maxW="3xl">
+    <Box padding="4" maxW="3xl" lineHeight="200%">
       <b>Thank you for completing our survey(s)!</b>
       <br />
-      <br />
       We appreciate your time.
-      <br />
       <br />
       <br />
       <Button colorScheme="teal" size="md">
