e15224ad3b82bf16d37f4fc542e749d0f15c8b26
onb6 <blierolivia@gmail.com>
2022-01-04 11:41:33 -0500

    move admin table to own component

---
+++ src/components/researcherLandingPage/AdminTableRow.tsx
@@ -0,0 +1,17 @@
+import { Td, Tr } from '@chakra-ui/react';
+import React from 'react';
+import User from '../../api/dtos/user.dto';
+
+export interface AdminTableRowProps {
+  admin: User;
+}
+
+const AdminTableRow: React.FC<AdminTableRowProps> = ({ admin }) => (
+  <Tr>
+    <Td>Placeholder name</Td>
+    <Td>{admin.email}</Td>
+    <Td>Placeholder date</Td>
+  </Tr>
+);
+
+export default AdminTableRow;
