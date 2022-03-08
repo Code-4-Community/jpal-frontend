1c0a60358beed8a6705ca704768c61e93cb46adb
onb6 <blierolivia@gmail.com>
2022-03-08 10:58:10 -0500

    remove exit button

--- src/components/survey/ThankYou.tsx
+++ src/components/survey/ThankYou.tsx
@@ -3,15 +3,10 @@ import { Box, Button, VStack } from '@chakra-ui/react';
 
 const ThankYou: React.FC = () => (
   <VStack justify="center" height="full">
-    <Box padding="4" maxW="3xl" lineHeight="200%">
+    <Box padding="16" maxW="3xl" lineHeight="200%">
       <b>Thank you for completing our survey(s)!</b>
       <br />
-      We appreciate your time.
-      <br />
-      <br />
-      <Button colorScheme="teal" size="md">
-        Exit
-      </Button>
+      We appreciate your time. You may now close this tab.
     </Box>
   </VStack>
 );
