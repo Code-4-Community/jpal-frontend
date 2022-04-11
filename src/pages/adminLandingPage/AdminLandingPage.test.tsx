4e8e25fcc6042da0db2ea15727b1dbfc707d0ca6
Atulya Miglani <atulyamiglani02@gmail.com>
2022-04-11 00:14:27 -0400

    add props to the tests

--- src/pages/adminLandingPage/AdminLandingPage.test.tsx
+++ src/pages/adminLandingPage/AdminLandingPage.test.tsx
@@ -31,14 +31,14 @@ const surveyList: Survey[] = [
 
 describe('Admin Landing Page', () => {
   test('renders create survey button', () => {
-    render(<AdminLandingPage />);
+    render(<AdminLandingPage userAvatarClicked={false} />);
     const createSurveyButton = screen.getByText('Create Survey');
     expect(createSurveyButton).toBeInTheDocument();
   });
 
   test('renders survey table', async () => {
     apiClient.getMySurveys = jest.fn(() => Promise.resolve(surveyList)) as () => Promise<Survey[]>;
-    render(<AdminLandingPage />);
+    render(<AdminLandingPage userAvatarClicked={false} />);
     const surveyName = await screen.findByText('Survey Name');
     const surveyDate = await screen.findByText('Date Created');
     expect(surveyName).toBeInTheDocument();
