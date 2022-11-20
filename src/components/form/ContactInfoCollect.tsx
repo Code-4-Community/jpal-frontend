827f0d0c65bfbb87592f8c40fa693403a22249a2
DerekL27 <dleung2140@gmail.com>
2022-11-20 14:36:02 -0500

    fixed linter errors

--- src/components/form/ContactInfoCollect.tsx
+++ src/components/form/ContactInfoCollect.tsx
@@ -21,9 +21,9 @@ const alertOnSubmit = async (values: FormValues) => {
 
 const validateEmail = (value: string) => {
   let error: string | undefined;
-  const emailRegex = "^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$";
-  if(value.match(emailRegex) == null) {
-    error = "Invalid email address";
+  const emailRegex = '^[a-zA-Z0-9]+(?:.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:.[a-zA-Z0-9]+)*$';
+  if (value.match(emailRegex) == null) {
+    error = 'Invalid email address';
   }
   return error;
 };
@@ -31,28 +31,23 @@ const validateEmail = (value: string) => {
 const validatePhoneNumber = (value: string) => {
   let error: string | undefined;
   const phoneNumberRegex = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-  if(value.match(phoneNumberRegex) == null) {
-    error = "Invalid phone number";
+  if (value.match(phoneNumberRegex) == null) {
+    error = 'Invalid phone number';
   }
   return error;
 };
 
 const ContactInfoCollect: React.FC<ContactFormProps> = ({ onSubmit }) => (
-    // eslint-disable-next-line no-alert
-    <Form onSubmit={onSubmit ?? alertOnSubmit}>
-      <InputField
-          isRequired
-          displayName="Email"
-          fieldName="email"
-          validate={validateEmail}
-      />
-      <InputField
-          isRequired
-          displayName="Phone Number"
-          fieldName="phoneNumber"
-          validate={validatePhoneNumber}
-      />
-    </Form>
+  // eslint-disable-next-line no-alert
+  <Form onSubmit={onSubmit ?? alertOnSubmit}>
+    <InputField isRequired displayName="Email" fieldName="email" validate={validateEmail} />
+    <InputField
+      isRequired
+      displayName="Phone Number"
+      fieldName="phoneNumber"
+      validate={validatePhoneNumber}
+    />
+  </Form>
 );
 
 export default ContactInfoCollect;
