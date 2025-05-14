389e55f2eae3959f4bf24f0c9d4b3d15d1bd2db0
Avery Huang <averyhuang2003@gmail.com>
2025-05-14 16:52:41 -0400

    oops

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
-// Map with all the roles mapped to their landing pages
-
 import Role from './api/dtos/role';
 
+// Map with all the roles mapped to their landing pages
 // eslint-disable-next-line import/prefer-default-export
 export const roleMap = {
   [Role.ADMIN]: '/private',
